cea791da6df2fb5f1eb4b942c0361c275178808b
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-06-17 08:56:13 +0100

    Add password visibility icon

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,11 @@
         <v-form>
           <v-text-field label="Username" prepend-icon="mdi-account-circle" />
           <v-text-field
-            type="password"
+            :type="showPassword ? 'text' : 'password'"
             label="Password"
             prepend-icon="mdi-lock"
-            append-icon="mdi-eye-off"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
           />
         </v-form>
       </v-card-text>
@@ -32,7 +33,7 @@ export default {
   components: {},
 
   data: () => ({
-    //
+    showPassword: false,
   }),
 };
 </script>
