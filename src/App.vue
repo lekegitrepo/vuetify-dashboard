98d1e65b6ad648c3206648a5fc092d7f7534502f
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-06-17 08:50:37 +0100

    Add icons

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <v-app>
-    <v-card>
+    <v-card width="400" class="mx-auto mt-5">
       <v-card-title>
         <h1 class="display">Login</h1>
       </v-card-title>
       <v-card-text>
         <v-form>
-          <v-text-field label="Username" />
-          <v-text-field type="password" label="Password" />
+          <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+          <v-text-field
+            type="password"
+            label="Password"
+            prepend-icon="mdi-lock"
+            append-icon="mdi-eye-off"
+          />
         </v-form>
       </v-card-text>
+      <v-divider></v-divider>
       <v-card-actions>
-        <v-btn>Register</v-btn>
-        <v-btn>Login</v-btn>
+        <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Login</v-btn>
       </v-card-actions>
     </v-card>
   </v-app>
