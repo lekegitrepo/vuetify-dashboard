38550b64ddde941b391a1d6d8566bc4466787a7e
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-06-21 08:29:45 +0100

    Create Login component

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,7 @@
       <v-btn text rounded>Home</v-btn>
       <v-btn text rounded>Login</v-btn>
     </v-app-bar>
-    <v-content>
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-content>
+    <v-content></v-content>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
         <v-btn
@@ -58,7 +34,6 @@ export default {
   components: {},
 
   data: () => ({
-    showPassword: false,
     links: ["Home", "Login"],
   }),
 };
