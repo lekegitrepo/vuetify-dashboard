d3b7d57f7e17e96d2b46cdd2433a521c43f9caac
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-06-21 08:55:07 +0100

    Create dashboard component

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <template>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :items-per-page="5"
+        class="elevation-1"
+      ></v-data-table>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
